4eae08efd4311a9e26961201397bd3dd36a47cdf
Rayssa de Freitas <rayssamf@gmail.com>
2021-12-09 16:00:31 -0300

    Começando a parte da pagina inicial

--- front-end/src/app/pages/deslogado/deslogado.module.ts
+++ front-end/src/app/pages/deslogado/deslogado.module.ts
@@ -3,17 +3,29 @@ import { CommonModule } from '@angular/common';
 import { DeslogadoRoutingModule } from './deslogado.routing';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { InicialComponent } from './inicial/inicial.component';
+import { VendaComponent } from './venda/venda.component';
+import { ContatoComponent } from './contato/contato.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
 @NgModule({
   declarations: [
-    InicialComponent
+    InicialComponent,
+    VendaComponent,
+    ContatoComponent
   ],
   imports: [
     CommonModule,
     DeslogadoRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    //AngularMaterial
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
